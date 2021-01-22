a37a0ad01e968238b6675547c072182b9514d316
Zabehalin <z380966645657@gmail.com>
2021-01-22 17:20:23 +0200

    PROXY

--- start.py
+++ start.py
@@ -8,44 +8,52 @@ import re
 
 
 def main():
-    vys = False
+    # =============== User Agent =====================
     U_ag = random.choice(list(open('user-agents.txt')))
     U_ag = U_ag[0:-1]
-
+    # ================================================
     # --| Setup
     if "Macintosh;" in U_ag or "Windows" in U_ag:
+        # =============== PROXY ==========================
         prox = random.choice(list(open('http.txt')))
-        sg = prox
-        res = re.sub(r'\:.*', "", prox)
-        res = res[0:-1]
-        #sg = prox
-        temp = sg.find(':')
-        index = temp
-        sg = sg[index:]
-        sg = sg[1:]
-        sg = sg[0:-1]
-        print("PROXI = ", res, "port = ", sg)
+        prox = prox[0:-1]
+        print(prox)
+
         opt = Options()
         opt.headless = False
         options = webdriver.FirefoxProfile()
-        options.set_preference("network.proxy.type", 1)
-        options.set_preference("network.proxy.http", res)
-        options.set_preference("network.proxy.http_port", int(sg))
+        firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+        firefox_capabilities["marionette"] = True
+
+        firefox_capabilities["proxy"] = {
+            "proxyType": "MANUAL",
+            "httpProxy": prox,
+            "ftpProxy": prox,
+            "sslProxy": prox
+        }
+        # ===========================================================
+        # =============== User Agent ==========================
         options.set_preference("general.useragent.override", U_ag)
-
-        options.update_preferences()
         print(U_ag)
+        # =====================================================
+        # ======================================================
+        browser = webdriver.Firefox(
+            firefox_profile=options, options=opt, proxy=prox)
+        # =======================================================
+        # browser.get('https://2ip.ru/')
+        # ============Configure browser=======================
+        # Youtube Канал
+        browser.get('https://www.youtube.com/watch?v=mNEc3MtdCuQ')
+        element = browser.find_element_by_class_name(
+            "ytp-mute-button")  # Натиснуть кнопку звуку (Виключить звук)
+        element.click()
+        element = browser.find_element_by_class_name(
+            "ytp-play-button")  # Натиснуть кнопку Play
+        element.click()
+        # ======================================================
 
-        browser = webdriver.Firefox(firefox_profile=options, options=opt)
-        browser.get('https://2ip.ru/')
-        # browser.get('https://www.youtube.com/watch?v=mNEc3MtdCuQ')
-        # element = browser.find_element_by_class_name("ytp-mute-button")
-        # element.click()
-        # element = browser.find_element_by_class_name("ytp-play-button")
-        # element.click()
-
-        time.sleep(10)
-        browser.close()
+        time.sleep(5)  # =======Затримка
+        browser.close()  # =========Закрить браузер
 
 
 for item in range(1):
